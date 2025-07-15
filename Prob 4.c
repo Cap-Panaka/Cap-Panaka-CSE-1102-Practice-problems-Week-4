c200a45d27908712a4e789112f5d86a292bf9e8b
Cap-Panaka <nerdcrab1122@gmail.com>
2025-07-15 12:08:20 +0600

    Update Prob 4.c

--- Prob 4.c
+++ Prob 4.c
@@ -1,17 +1,20 @@
 //Write a C program that will print the sum of the following series.  1+3+...+(2n+1)
 #include <stdio.h>
-int main() {
-    int i,n,sum=0;
-    printf("Enter the value of n: ");
-    scanf(" %d",&n);
-    for(i=1;i<=(2*n+1);i+=2){
-        printf(" %d",i);
-        sum+=i;
 
-        if(i<2*n+1){
+int main() {
+    int i, n, sum = 0;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    printf("Series: ");
+    for (i = 1; i <= (2 * n + 1); i += 2) {
+        if (i > 1) {
             printf(" + ");
         }
+        printf("%d", i);
+        sum += i;
     }
-    printf(" = %d\n",sum);
+    printf(" = %d\n", sum);
+
     return 0;
 }
